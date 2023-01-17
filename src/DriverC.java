055f344a0c39416bf35efe17aed8e8addd684803
ruslankorotkov <ruslankorotkov.rus@yandex.ru>
2023-01-17 23:20:36 +0300

    Работа с исключениями 16/01/2023

---
+++ src/DriverC.java
@@ -0,0 +1,27 @@
+public class DriverC extends Driver {
+    public DriverC(String name, boolean driverLicense, String experience) {
+        super(name, driverLicense, experience);
+        if (driverLicense == true) {
+            this.driverLicense = driverLicense;
+        } else if (driverLicense == false) {
+            throw new IllegalArgumentException(" Необходимо указать тип прав! .");
+        }
+    }
+
+    @Override
+    public void startMove() {
+        System.out.println("Водитель категории C " + getName() + " НАЧАЛ ДВИЖЕНИЕ ");
+
+    }
+
+    @Override
+    public void stopMove() {
+        System.out.println("Водитель категории C " + getName() + " ОСТАНОВИЛСЯ ");
+    }
+
+    @Override
+    public void refuel() {
+        System.out.println("Водитель категории C " + getName() + " ЗАПРАВИЛ АВТО ");
+    }
+
+}
